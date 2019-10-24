8b1e4e9f4261d4d55e198ef079283f918a4cb14c
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-24 03:09:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,5 @@ end
 
 def find_the_cheese(foods)
   array_of_cheeses=["cheddar","gouda","camembert"]
-   array_of_cheeses.select {|i| foods.include? (i)}
-   foods.split (" ")
+   array_of_cheeses.select {|i| foods.include? (i).split (" ")}
 end
